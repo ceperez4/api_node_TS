dcfc9501d041d030ffd92f577a578a9f7b8c4e90
ceperez4 <cris18enrique@gmail.com>
2023-03-20 12:15:59 -0500

    Ruta 404 añadida

--- src/config/app.ts
+++ src/config/app.ts
@@ -8,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
+app.use((_req, res, _next) => {
+  res.status(404).json({ message: "Not found" });
+});
 
 db.then(() => {
   console.log("db is connected and ready");
